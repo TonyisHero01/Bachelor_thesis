741365c61da4bb13b8c135fa6fc83455d7297a6f
TonyisHero01 <t1509517798@gmail.com>
2026-07-04 12:25:09 +0200

    clear unused codes

--- benchmark/esci-checker.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_parquet("/esci-data/shopping_queries_dataset/shopping_queries_dataset_examples.parquet")
-
-print(df.head())
-print(df.columns)
-print(df[["query", "product_id", "esci_label"]].head(20))
