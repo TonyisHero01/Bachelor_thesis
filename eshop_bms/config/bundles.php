9366703c03f61cddf9d68cf8a8954d17a12ca041
TonyIsHero01 <t1509517798@gmail.com>
2026-07-15 16:19:07 +0200

    add openapi docs

--- eshop_bms/config/bundles.php
+++ eshop_bms/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
