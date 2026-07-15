315399d8c03409a09887ed7dafcb0751f50d7275
TonyIsHero01 <t1509517798@gmail.com>
2026-07-15 23:24:39 +0200

    update test bootstrap

--- eshop_frontweb/tests/bootstrap.php
+++ eshop_frontweb/tests/bootstrap.php
@@ -1,13 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+$envFile = dirname(__DIR__).'/.env';
+
+if (method_exists(Dotenv::class, 'bootEnv') && is_file($envFile)) {
+    (new Dotenv())->bootEnv($envFile);
 }
 
-if ($_SERVER['APP_DEBUG']) {
+$appDebug = $_SERVER['APP_DEBUG']
+    ?? $_ENV['APP_DEBUG']
+    ?? getenv('APP_DEBUG')
+    ?? false;
+
+if (filter_var($appDebug, FILTER_VALIDATE_BOOL)) {
     umask(0000);
 }
